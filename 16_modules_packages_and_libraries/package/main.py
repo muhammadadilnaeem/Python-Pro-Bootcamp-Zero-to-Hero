3eb444f44150e629f42e5f739922c306bf88a3a5
Muhammad Adil Naeem <162784706+muhammadadilnaeem@users.noreply.github.com>
2024-12-17 17:18:31 +0500

    main python file

---
+++ 16_modules_packages_and_libraries/package/main.py
@@ -0,0 +1,12 @@
+
+import main_utils
+
+# from main_utils import *
+
+print("\n-------------")
+print(main_utils.add(5,7))
+print("\n-------------")
+print(main_utils.subtract(10,7))
+print("\n-------------")
+print(main_utils.multiply(5000,7))
+print("\n-------------")
